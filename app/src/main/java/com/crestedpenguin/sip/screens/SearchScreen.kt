d6e74f2450ee20fd5ac1f96df7b39c78c71c9ea4
sudokube <@>
2024-05-14 20:56:04 +0900

    Add LazyColumn connection with Firebase

--- app/src/main/java/com/crestedpenguin/sip/screens/SearchScreen.kt
+++ app/src/main/java/com/crestedpenguin/sip/screens/SearchScreen.kt
@@ -8,43 +8,55 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
 @Composable
 fun SearchScreen() {
     // Accesss a Cloud Firestore instance from your Activity
     val db = Firebase.firestore
+    var supplementList by remember {
+        mutableStateOf<List<DocumentSnapshot>>(emptyList())
+    }
 
-    db.collection("supplements")
-        .get()
-        .addOnSuccessListener { result ->
-            for (document in result) {
-                Log.d(TAG, "${document.id} = ${document.data}")
-            }
-        }
-        .addOnFailureListener { exception ->
-            Log.w(TAG, "Error getting documents.", exception)
-        }
+    LaunchedEffect(Unit) {
+        supplementList =
+            db.collection("supplements")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.d(TAG, "${document.id} = ${document.data}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
+                .await()
+                .documents
+    }
 
     LazyColumn(
         modifier = Modifier
             .padding(horizontal = 8.dp)
             .fillMaxSize()
     ) {
-        item {
-            SupplementItem(product = "Example1")
-            SupplementItem(product = "Example2")
-            SupplementItem(product = "Example3")
-            SupplementItem(product = "Example4")
-            SupplementItem(product = "Example5")
+        itemsIndexed(supplementList) { _, document ->
+            document.getString("company")?.let { SupplementItem(product = it) }
         }
     }
 }
