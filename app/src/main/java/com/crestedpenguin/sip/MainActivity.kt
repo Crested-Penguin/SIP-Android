3ca00485fe2145ec142b9c24e5d966fb1f5b9a3d
sudokube <@>
2024-05-29 18:23:44 +0900

    Fix BottomAppBar IconButton align

--- app/src/main/java/com/crestedpenguin/sip/MainActivity.kt
+++ app/src/main/java/com/crestedpenguin/sip/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -16,14 +14,10 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -37,7 +31,6 @@ import com.crestedpenguin.sip.screens.SupplementScreen
 import com.crestedpenguin.sip.ui.SupplementViewModel
 
 class MainActivity : AppCompatActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -47,8 +40,11 @@ class MainActivity : AppCompatActivity() {
 
             Scaffold(
                 bottomBar = {
-                    BottomAppBar(
-                        actions = {
+                    BottomAppBar {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
                             IconButton(onClick = { navController.navigate("home") }) {
                                 Icon(Icons.Filled.Home, contentDescription = "Home")
                             }
@@ -62,8 +58,8 @@ class MainActivity : AppCompatActivity() {
                                 Icon(Icons.Filled.Settings, contentDescription = "Settings")
                             }
                         }
-                    )
-                },
+                    }
+                }
             ) { innerPadding ->
                 NavHost(
                     navController = navController,
@@ -80,29 +76,6 @@ class MainActivity : AppCompatActivity() {
                     composable(Screen.Supplement.route) { SupplementScreen(supplementViewModel = supplementViewModel) }
                     composable(Screen.Star.route) { StarScreen() }
                     composable(Screen.Settings.route) { SettingsScreen() }
-                }
-                if (supplementViewModel.showBottomSheet == true) {
-                    ModalBottomSheet(
-                        onDismissRequest = { supplementViewModel.showBottomSheet = false },
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Column(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxSize(),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceAround
-                            ) {
-                                FilterChip(target = "isWPC")
-                                FilterChip(target = "isWPI")
-                                FilterChip(target = "isWPH")
-                            }
-                        }
-                    }
                 }
             }
         }
